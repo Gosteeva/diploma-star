db6acd5b9aa8fefbd62079caecc6e1d76fa65980
Gosteeva <olia.gosteeva@gmail.com>
2016-06-26 00:26:58 +0300

    base

---
+++ src/js/search-btn.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.tg').click(function() {
+      $('.bttn').toggle();
+      $('.search').toggle();
+      $('.s-txt').toggle();
+      $('.l-txt').toggle();
+     })
+});
