520ed0dd0f6a062af14610cba7b4224724e1b86d
Gosteeva <olia.gosteeva@gmail.com>
2018-07-13 12:49:41 +0300

    Add files via upload

---
+++ file_samples/Custom header sample.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" x-crowdin-source-element="translation" sourcelanguage="en">
+  <context>
+    <name>AddBridgeScreen</name>
+    <message>
+      <source>Text</source>
+      <translation>Text for translation</translation>
+    </message>
+    <message>
+      <source>text_2</source>
+      <translation>Text for translation 2</translation>
+    </message>
+  </context>
+</TS>
